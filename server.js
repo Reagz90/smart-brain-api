715d41292c9174bcfa1547d3c16fa5d03e6003cd
Reagz <44364513+Reagz90@users.noreply.github.com>
2023-07-24 18:20:32 +1000

    first commit, backend code for smartbrain app

---
+++ server.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const cors = require("cors")
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//as we do not have a database yet, using an array to test
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+//root
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+// SIGN IN
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json(database.users[0]);
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
+
+// REGISTER
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password, 
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+// PROFILE LOOKUP
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+});
+
+//UPDATE RANK ON IMAGE SUBMISSION
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("user not found");
+  }
+  console.log('USER ID PASSED TO IMAGE ENDPOINT-->', id)
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
+
+// PASSWORD ENCRYPTION FUNCTIONS
+// bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+//   // Store hash in your password DB.
+// });
+
+// // Load hash from your password DB.
+// bcrypt.compare(myPlaintextPassword, hash, function (err, result) {
+//   // result == true
+// });
+// bcrypt.compare(someOtherPlaintextPassword, hash, function (err, result) {
+//   // result == false
+// });
