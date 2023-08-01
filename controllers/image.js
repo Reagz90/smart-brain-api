31b7d454a793d9c3af1584b0331c29f5f23c10e3
Reagz <44364513+Reagz90@users.noreply.github.com>
2023-08-01 21:52:25 +1000

    moved clarifai api call from frontend to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,43 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 876f3f1773944f1da373eb31e465b2fb");
+
+const handleApiCall = (req, res) => {
+  stub.PostModelOutputs(
+    {
+        user_app_id: {
+          user_id: "clarifai",  
+          app_id: "main"
+        },
+      model_id: "face-detection",
+      inputs: [
+        { data: { image: { url: req.body.input } } },
+      ],
+    },
+    metadata,
+    (err, response) => {
+      if (err) {
+        console.log("Error: " + err);
+        return;
+      }
+
+      if (response.status.code !== 10000) {
+        console.log(
+          "Received failed status: " +
+            response.status.description +
+            "\n" +
+            response.status.details
+        );
+        return;
+      }
+      res.json(response);
+    }
+  );
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -12,4 +52,5 @@ const handleImage = (req, res, db) => {
 
 module.exports = {
   handleImage,
+  handleApiCall
 };
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 const app = express();
 const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require ('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
@@ -25,20 +25,32 @@ app.use(cors());
 
 //root
 app.get("/", (req, res) => {
-  res.send('success');
+  res.send("success");
 });
 
 // SIGN IN
-app.post("/signin", (req, res) => { signin.handleSignIn(req, res, db, bcrypt) });
+app.post("/signin", (req, res) => {
+  signin.handleSignIn(req, res, db, bcrypt);
+});
 
 // REGISTER
-app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
 
 // PROFILE LOOKUP
-app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
 
 //UPDATE RANK ON IMAGE SUBMISSION
-app.put("/image", (req, res) => { image.handleImage(req, res, db) });
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
